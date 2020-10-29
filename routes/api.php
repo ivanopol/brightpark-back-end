f1933d57aa5e364d909a50316785d0b844bd2c66
ivanopol <ivanopol777@mail.ru>
2020-10-29 16:24:45 +0500

    Добавил кеширование для роутов

--- routes/api.php
+++ routes/api.php
@@ -13,31 +13,29 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['cache.headers:private;max_age=3600'])->group(function () {
+    Route::get('/model_details', 'API\ModelController@model_details')->name('model_details');
+    Route::get('/models_list', 'API\ModelController@index')->name('models_list');
+    Route::get('/carcasses', 'API\ModelController@carcasses')->name('carcasses');
+    Route::get('/model', 'API\ModelController@model')->name('model');
+    Route::get('/get_cars_list', 'API\ModelController@getCarsList');
+    Route::get('/get_cars_brands', 'API\ModelController@getCarsBrands');
+    Route::get('/retargeting', 'API\ModelController@getRetargetingInfo');
+    Route::get('/get_cities', 'GeoController@get_cities');
+    Route::get('/news', 'API\ModelController@getNewsList');
+    Route::get('/stocks', 'API\ModelController@getStocksList');
+    Route::get('/stocks_item', 'API\ModelController@getStocksItem');
+    Route::get('/news_item', 'API\ModelController@getNewsItem');
+    Route::post('/get_meta_tags', 'API\ModelController@getMetaTags');
+
+    Route::get('/get_city_by_ip', 'GeoController@get_user_city_by_ip');
+    Route::get('/get_brand_models', 'ModelController@get_brand_models');
+    Route::post('/get_estimation', 'ModelController@getEstimation');
+    Route::get('/get_complectations/{brand_id}/{model_id}', 'ModelController@getComplectations');
+    Route::get('/get_credit_programs', 'ModelController@getCreditPrograms');
+    Route::post('/write_event', 'EventController@write_event');
+
+    Route::post('/send_contact_form', 'ContactFormController@sendContactForm')->name('sendContactForm');
 });
-
-Route::get('/model_details', 'API\ModelController@model_details')->name('model_details');
-Route::get('/models_list', 'API\ModelController@index')->name('models_list');
-Route::get('/carcasses', 'API\ModelController@carcasses')->name('carcasses');
-Route::get('/model', 'API\ModelController@model')->name('model');
-Route::get('/get_cars_list', 'API\ModelController@getCarsList');
-Route::get('/get_cars_brands', 'API\ModelController@getCarsBrands');
-Route::get('/retargeting', 'API\ModelController@getRetargetingInfo');
-Route::get('/get_cities', 'GeoController@get_cities');
-Route::get('/news', 'API\ModelController@getNewsList');
-Route::get('/stocks', 'API\ModelController@getStocksList');
-Route::get('/stocks_item', 'API\ModelController@getStocksItem');
-Route::get('/news_item', 'API\ModelController@getNewsItem');
-Route::post('/get_meta_tags', 'API\ModelController@getMetaTags');
-
-Route::get('/get_city_by_ip', 'GeoController@get_user_city_by_ip');
-Route::get('/get_brand_models', 'ModelController@get_brand_models');
-Route::post('/get_estimation', 'ModelController@getEstimation');
-Route::get('/get_complectations/{brand_id}/{model_id}', 'ModelController@getComplectations');
-Route::get('/get_credit_programs', 'ModelController@getCreditPrograms');
-Route::post('/write_event', 'EventController@write_event');
-
-Route::post('/send_contact_form', 'ContactFormController@sendContactForm')->name('sendContactForm');
 
 
