47912ee58f5bd5389038c76ef013945953d773dc
ivanopol <ivanopol777@mail.ru>
2021-04-13 16:57:45 +0500

    Добавил подменный контент для акций
    #stocks

--- app/Stocks.php
+++ app/Stocks.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\Types\Integer;
 
 class Stocks extends Model
 {
@@ -39,5 +40,38 @@ class Stocks extends Model
     public function getRouteKeyName(): string
     {
         return 'slug';
+    }
+
+    /**
+     * Производим замену подстановочных данных в тексте
+     *
+     *
+     */
+    public static function handlePlaceholders (object $data, String $city_id, array $needle) : object
+    {
+        $city = City::find($city_id);
+        $contacts = Contacts::where('city_id', $city_id)->first();
+
+        $patterns = [
+            '/<:CITY:>/',
+            '/<:CITY_DATIVE:>/',
+            '/<:REGION_DATIVE:>/',
+            '/<:ADDRESS:>/',
+            '/<:PHONE:>/'
+        ];
+
+        $replacements = [
+            $city->title_ru,
+            $city->city_dative,
+            $city->region_dative,
+            $contacts->address,
+            $contacts->phone
+        ];
+
+        foreach ($needle as $target_row) {
+            $data[$target_row] = preg_replace($patterns, $replacements, $data[$target_row]);
+        }
+
+        return $data;
     }
 }
