f98bca6d8edfb44cad8fabf16d8f3394f1b857c7
ivanopol <ivanopol777@mail.ru>
2021-06-09 16:54:14 +0500

    Добавил дополнительные поля для Тест-драйва #fix
    #test-drive #form

--- app/Services/BitrixService.php
+++ app/Services/BitrixService.php
@@ -111,7 +111,7 @@ class BitrixService
             }
         });
 
-        // Ищем есть ли лиды или контакты с таким номером в Битриксе, если есть
+// Ищем есть ли лиды или контакты с таким номером в Битриксе, если есть
         // Ставим признак "Повторный лид"
         $curl = curl_init();
         curl_setopt_array($curl, [
@@ -122,19 +122,11 @@ class BitrixService
             CURLOPT_POSTFIELDS => http_build_query($request),
         ]);
 
-        $resultDuplicate = curl_exec($curl);
+        $res = curl_exec($curl);
         curl_close($curl);
-        $resultDuplicate = json_decode($resultDuplicate, 1);
+        $res = json_decode($res, 1);
 
-        $contactID = 0;
-        if (!empty($resultDuplicate['result'])) {
-            if (isset($resultDuplicate['result']["CONTACT"]) && !empty($resultDuplicate['result']["CONTACT"])) {
-                $count = count($resultDuplicate['result']["CONTACT"]) - 1;
-                $contactID = $resultDuplicate['result']["CONTACT"][$count];
-            }
-        }
-
-        $isDuplicate = intval($contactID) > 0;
+        $isDuplicate = count($res["result"]) > 0;
 
         // Определение ответственного из сотрудниц ЕРЦ которые онлайн
         $department_id = $data['form_type'] === 2 ? 1552 : 834;
@@ -170,6 +162,8 @@ class BitrixService
 
         $info = 'Создан новый лид #ID_SUSH# и к нему прикреплено дело #ID_JOB#';
 
+        $isReturnCustomer = $isDuplicate ? "Y" : "N";
+
         if ($url) {
             $url = explode('?', $url);
             $urlArr = explode('//', $url[0]);
@@ -193,13 +187,10 @@ class BitrixService
                 "SOURCE_ID" => "SELF",
                 "NAME" => $data['name'], //имя из поля
                 "PHONE" => [["VALUE" => $phone, "VALUE_TYPE" => "MOBILE"]],
+                "IS_RETURN_CUSTOMER" => $isReturnCustomer,
             ],
             'params' => ["REGISTER_SONET_EVENT" => "Y"],
         ];
-
-        if ( $isDuplicate ) {
-            $request['fields']['CONTACT_ID'] = $contactID;
-        }
 
         if (isset($data['utm']) && count($data['utm'])) {
             foreach (['utm_campaign', 'utm_content', 'utm_medium', 'utm_source', 'utm_term', 'block', 'source', 'yclid'] as $label) {
