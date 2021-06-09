5f520c14e58fde84e62cb2772a3bc7f8d25e3eed
ivanopol <ivanopol777@mail.ru>
2021-06-09 17:04:16 +0500

    Добавил дополнительные поля для Тест-драйва #fix
    #test-drive #form

--- resources/views/emails/feedback.blade.php
+++ resources/views/emails/feedback.blade.php
@@ -3,6 +3,12 @@
 <ul>
     <li>Имя: <?php echo $name; ?></li>
     <li>Телефон: <?php echo $phone; ?></li>
+    @if($date)
+        <li>Дата: {{$date}} <?php echo ($time) ? ", " . $time : ""; ?></li>
+    @endif
+    @if($car)
+        <li>Автомобиль: {{$car}}</li>
+    @endif
     <li>Город: <?php echo $city; ?></li>
     <li><a href="<?php echo $url; ?>"><?php echo $url; ?></a></li>
     <li>Комментарий: <?php echo $comment; ?></li>
